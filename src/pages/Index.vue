c10219113bdbd1c3d2aadba84ab8cad49953b054
unknown <composer03@3D003.studio.local>
2021-07-24 15:22:27 +0300

    feat: Добавить страницу Index

---
+++ src/pages/Index.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-row>
+        <v-col
+            cols="4"
+        >
+            <v-card
+                outlined
+            >
+                <v-card-title>Фильтры</v-card-title>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="8"
+        >
+            <v-card
+                outlined
+            >
+                <v-card-title>Работа с сообщениями</v-card-title>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="4"
+        >
+            <v-card
+                outlined
+            >
+                <v-card-title>Список сообщений</v-card-title>
+            </v-card>
+        </v-col>
+        <v-col
+            cols="8"
+        >
+            <v-card
+                outlined
+            >
+                <v-card-text>1</v-card-text>
+            </v-card>
+        </v-col>
+    </v-row>
+</template>
+
+<script>
+  export default {
+    name: 'Index',
+
+    data: () => ({
+      
+    }),
+  }
+</script>
